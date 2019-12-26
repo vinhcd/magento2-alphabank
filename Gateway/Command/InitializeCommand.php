dc5c2794e0d686feb5eeec365418faa5a107754e
hungnt <hungnt@bsscommerce.com>
2019-12-26 09:54:35 +0700

    add command facade

---
+++ Gateway/Command/InitializeCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Monogo\Alphabank\Gateway\Command;
+
+use Magento\Payment\Gateway\CommandInterface;
+use Magento\Payment\Gateway\Helper\ContextHelper;
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Payment;
+
+class InitializeCommand implements CommandInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function execute(array $commandSubject)
+    {
+        $stateObject = SubjectReader::readStateObject($commandSubject);
+        $paymentDO = SubjectReader::readPayment($commandSubject);
+
+        /** @var Payment $payment */
+        $payment = $paymentDO->getPayment();
+        ContextHelper::assertOrderPayment($payment);
+
+        $payment->setAmountAuthorized($payment->getOrder()->getTotalDue());
+        $payment->setBaseAmountAuthorized($payment->getOrder()->getBaseTotalDue());
+        $payment->getOrder()->setCanSendNewEmailFlag(false);
+
+        $stateObject->setData(OrderInterface::STATE, Order::STATE_PENDING_PAYMENT);
+        $stateObject->setData(OrderInterface::STATUS, Order::STATE_PENDING_PAYMENT);
+        $stateObject->setData('is_notified', false);
+    }
+}
--- Gateway/Validator/ResponseCodeValidator.php
+++ Gateway/Validator/ResponseCodeValidator.php
@@ -2,7 +2,44 @@
 
 namespace Monogo\Alphabank\Gateway\Validator;
 
-class ResponseCodeValidator
-{
+use Magento\Payment\Gateway\Validator\AbstractValidator;
+use Magento\Payment\Gateway\Validator\ResultInterface;
 
+class ResponseCodeValidator extends AbstractValidator
+{
+    /**
+     * Performs validation of result code
+     *
+     * @param array $validationSubject
+     * @return ResultInterface
+     */
+    public function validate(array $validationSubject)
+    {
+        if (!isset($validationSubject['response']) || !is_array($validationSubject['response'])) {
+            throw new \InvalidArgumentException('Response does not exist');
+        }
+
+        $response = $validationSubject['response'];
+
+        if ($this->isSuccessfulTransaction($response)) {
+            return $this->createResult(
+                true,
+                []
+            );
+        } else {
+            return $this->createResult(
+                false,
+                [__('Gateway rejected the transaction.')]
+            );
+        }
+    }
+
+    /**
+     * @param array $response
+     * @return bool
+     */
+    private function isSuccessfulTransaction(array $response)
+    {
+        return true; //todo: implement
+    }
 }
